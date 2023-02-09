3d67b7ab73eaae1d5fe4c79c59175c7cc1f63fb0
crisG01 <gue22742@uvg.edu.gt>
2023-02-08 19:02:15 -0600

    Agrego el quick

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
     // run-time complexity = O(n Log n)
     // space complexity    = O(n)
-    
+    Quick q = new Quick();
     MergeSort m = new MergeSort();
     
     int[] array = {10, 50, 1, 0, 40, 15, 64};
@@ -13,6 +13,10 @@ public class Main {
     for(int i = 0; i < array.length; i++){
         System.out.print(array[i]+ " ");
     }
+    q.quicksort(array, 0, array.length - 1);
+ 
+    
+     System.out.println("\n"+Arrays.toString(array));
     }
 
 	
